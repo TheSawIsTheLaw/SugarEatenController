4dbd6881707ee70560510b4c05518c3a55be2db0
TheSawIsTheLaw <dimakrok@bk.ru>
2021-09-16 00:55:49 +0300

    ADD: starts of logic with preferences

--- SugarEatenController/app/src/main/java/com/example/sugareatencontroller/MainMenu.kt
+++ SugarEatenController/app/src/main/java/com/example/sugareatencontroller/MainMenu.kt
@@ -1,16 +1,45 @@
 package com.example.sugareatencontroller
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.sugareatencontroller.databinding.FragmentMainMenuBinding
+import java.sql.Time
 
 class MainMenu : Fragment()
 {
     lateinit var binding: FragmentMainMenuBinding
+    lateinit var preferences: SharedPreferences
+
+    @SuppressLint("SetTextI18n")
+    fun preparePrevDay()
+    {
+        binding.prevDayInfo.text =
+            "${getString(R.string.previous_day)} ${preferences.getFloat("prevDayResult", 0.0f)}"
+    }
+
+    fun setNewDay()
+    {
+        val editor = preferences.edit()
+        editor.putFloat("prevDayResult", preferences.getFloat("currentDayResult", 0.0f))
+        editor.apply()
+        Toast.makeText(requireContext(), "Wow, new day!", Toast.LENGTH_SHORT).show()
+    }
+
+    fun addToCurrentSugarList(sugarAmount: Float)
+    {
+        val gotSetOfAmounts = preferences.getStringSet("currentSugarAmounts", mutableSetOf())
+        val gotSetOfTimes = preferences.getStringSet("currentTimesAmounts", mutableSetOf())
+
+        gotSetOfAmounts.add()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,9 +48,17 @@ class MainMenu : Fragment()
     {
         binding = FragmentMainMenuBinding.inflate(layoutInflater)
 
+        preferences = requireContext().getSharedPreferences("SugarInfo", Context.MODE_PRIVATE)
+
+        preparePrevDay()
+
         binding.EditCurrentDayButton.setOnClickListener {
             Navigation.findNavController(requireView())
                 .navigate(R.id.action_mainMenu_to_changeRecord)
+        }
+
+        binding.newDayButton.setOnClickListener {
+            setNewDay()
         }
 
         return binding.root

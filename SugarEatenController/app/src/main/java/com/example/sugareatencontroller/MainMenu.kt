c3a62df4669075e6e4731203a76807a17301043b
TheSawIsTheLaw <55848738+TheSawIsTheLaw@users.noreply.github.com>
2021-09-16 19:08:26 +0300

    ADD: workable lists in preferences. New features soon.

--- SugarEatenController/app/src/main/java/com/example/sugareatencontroller/MainMenu.kt
+++ SugarEatenController/app/src/main/java/com/example/sugareatencontroller/MainMenu.kt
@@ -3,15 +3,21 @@ package com.example.sugareatencontroller
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.navigation.Navigation
 import com.example.sugareatencontroller.databinding.FragmentMainMenuBinding
-import java.sql.Time
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
+import java.time.Instant
+import java.util.*
 
 class MainMenu : Fragment()
 {
@@ -19,28 +25,89 @@ class MainMenu : Fragment()
     lateinit var preferences: SharedPreferences
 
     @SuppressLint("SetTextI18n")
-    fun preparePrevDay()
+    fun preparePrevDayLabel()
     {
         binding.prevDayInfo.text =
             "${getString(R.string.previous_day)} ${preferences.getFloat("prevDayResult", 0.0f)}"
     }
 
+    @SuppressLint("SetTextI18n")
+    fun prepareCurrentAmountOfEatenLabel()
+    {
+        val curAmount = preferences.getFloat("currentAmountOfEaten", 0.0f)
+        binding.todaysResultLabel.text = "${getString(R.string.current_amount_of_eaten)} $curAmount"
+    }
+
     fun setNewDay()
     {
         val editor = preferences.edit()
-        editor.putFloat("prevDayResult", preferences.getFloat("currentDayResult", 0.0f))
+        editor.putFloat("prevDayResult", preferences.getFloat("currentAmountOfEaten", 0.0f))
+        editor.putFloat("currentAmountOfEaten", 0.0f)
+        editor.putString("valuesList", "")
+        editor.putString("timesList", "")
         editor.apply()
+
+        prepareCurrentAmountOfEatenLabel()
+        preparePrevDayLabel()
+
         Toast.makeText(requireContext(), "Wow, new day!", Toast.LENGTH_SHORT).show()
     }
 
-    fun addToCurrentSugarList(sugarAmount: Float)
+    fun updateCurrentAmountOfEatenPref(sugarList: MutableList<Float>)
     {
-        val gotSetOfAmounts = preferences.getStringSet("currentSugarAmounts", mutableSetOf())
-        val gotSetOfTimes = preferences.getStringSet("currentTimesAmounts", mutableSetOf())
+        var eaten = 0.0f
+        sugarList.forEach { eaten += it }
 
-        gotSetOfAmounts.add()
+        val editor = preferences.edit()
+        editor.putFloat("currentAmountOfEaten", eaten)
+        editor.apply()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun addToCurrentSugarList(sugarAmount: Float)
+    {
+        var valuesListJson = preferences.getString("valuesList", "")
+        var timesListJson = preferences.getString("timesList", "")
+        var valuesList: MutableList<Float> = mutableListOf()
+        var timesList: MutableList<Instant> = mutableListOf()
+        if (valuesListJson != "")
+        {
+            valuesList = Gson().fromJson(valuesListJson, valuesList.javaClass)
+            timesList = Gson().fromJson(timesListJson, timesList.javaClass)
+        }
+
+        valuesList.add(0, sugarAmount)
+        timesList.add(0, Instant.now())
+
+        valuesListJson = Gson().toJson(valuesList)
+        timesListJson = Gson().toJson(timesList)
+        val editor = preferences.edit()
+        editor.putString("valuesList", valuesListJson)
+        editor.putString("timesList", timesListJson)
+        editor.apply()
+
+        updateCurrentAmountOfEatenPref(valuesList)
+        prepareCurrentAmountOfEatenLabel()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun addNewRecord()
+    {
+        val newRec = binding.newRecordValueInput.text.toString().toFloatOrNull()
+        if (newRec != null)
+        {
+            addToCurrentSugarList(newRec)
+            Toast.makeText(requireContext(), "Successfully added", Toast.LENGTH_SHORT).show()
+        }
+        else
+        {
+            Toast.makeText(requireContext(), "Invalid input, please, try again", Toast.LENGTH_LONG)
+                .show()
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -50,7 +117,8 @@ class MainMenu : Fragment()
 
         preferences = requireContext().getSharedPreferences("SugarInfo", Context.MODE_PRIVATE)
 
-        preparePrevDay()
+        preparePrevDayLabel()
+        prepareCurrentAmountOfEatenLabel()
 
         binding.EditCurrentDayButton.setOnClickListener {
             Navigation.findNavController(requireView())
@@ -59,6 +127,10 @@ class MainMenu : Fragment()
 
         binding.newDayButton.setOnClickListener {
             setNewDay()
+        }
+
+        binding.addNewRecordButton.setOnClickListener {
+            addNewRecord()
         }
 
         return binding.root
